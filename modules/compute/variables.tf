d3912d383356a99b2d90d1c6855dfbfe8cc76292
girish.matta <girish.matta@avanade.com>
2021-01-27 19:41:35 +1100

    Added lab5

---
+++ modules/compute/variables.tf
@@ -0,0 +1,25 @@
+variable "vm_name" {
+  type    = string
+  default = "vm_name"
+}
+
+variable "subnet_id" {
+  type    = string
+  default = "subnet_id"
+}
+
+variable "location" {
+  type    = string
+  default = "australiaeast"
+}
+
+variable "rg_name" {
+  type    = string
+  default = "resource_group_name"
+}
+
+variable "admin_password" {
+  type    = string
+  default = "@zureVMs32!!"
+}
+
