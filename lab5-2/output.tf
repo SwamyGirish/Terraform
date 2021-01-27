e3630115da34cd3b1ded479dcaf01dc69f63a62d
girish.matta <girish.matta@avanade.com>
2021-01-27 19:45:03 +1100

    removed lab5 folders

--- lab5-2/output.tf
+++
@@ -1,9 +0,0 @@
-output "firewall-public-ip" {
-  value = azurerm_public_ip.fe-pip.ip_address
-}
-
-output "webserver-internal-ip" {
-  value = module.web-vm.vmnic_private_ip
-}
-
-
--- lab5-3/backend.tfvars
+++
@@ -1,4 +0,0 @@
-resource_group_name  = "Terra-rg"
-storage_account_name = "gmattaterrasa01"
-container_name       = "tfstate"
-key                  = "azurebackend-lab5-3.tfstate"
